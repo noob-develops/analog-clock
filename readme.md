d16d90f87c91d6bba30ad05d58eb03bac12c0a43
usman <kohistani5525@gmail.com>
2023-04-26 03:12:59 +0500

    align analog with digital time

---
+++ readme.md
@@ -0,0 +1,3 @@
+### An Analog Clock using scss and vanila js
+
+<!-- dont compile scss i've made many changes directily in style.css -->
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const hoursEl = document.querySelector('.arrow .arrow--1');
-const minutesEl = document.querySelector('.arrow .arrow--2');
-const secondsEl = document.querySelector('.arrow .arrow--3');
+const hoursEl = document.querySelector('.arrow .arrow--1')
+const minutesEl = document.querySelector('.arrow .arrow--2')
+const secondsEl = document.querySelector('.arrow .arrow--3')
 
 const updateClock = () => {
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  const hoursDeg = (hours / 12) * 360;
-  console.log(hours, minutes, seconds);
-  hoursEl.style.transform = `rotate(${hoursDeg}deg)`;
-  const minutesDeg = (minutes / 60) * 360;
-  minutesEl.style.transform = `rotate(${minutesDeg}deg)`;
-  const secondsDeg = (seconds / 60) * 360;
-  secondsEl.style.transform = `rotate(${secondsDeg}deg)`;
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  const hoursDeg = (hours / 12) * 360
+  console.log(hours, minutes, seconds)
+  hoursEl.style.transform = `rotate(${hoursDeg}deg)`
+  const minutesDeg = (minutes / 60) * 360
+  minutesEl.style.transform = `rotate(${minutesDeg}deg)`
+  const secondsDeg = (seconds / 60) * 360
+  secondsEl.style.transform = `rotate(${secondsDeg}deg)`
 
-  setTimeout(updateClock, 1000);
-};
+  setTimeout(updateClock, 1000)
+}
 
-updateClock();
+updateClock()
